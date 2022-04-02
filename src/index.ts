cab9c97e3a9603b998e51be84190047599aa0379
Caleb Denio <cjdenio44@gmail.com>
2022-04-01 23:10:12 -0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import axios from "axios";
+import { DateTime } from "luxon";
 
 const app = express();
 const server = http.createServer(app);
 
-const EVENT_KEY = "2022macma";
-const TEAM_KEY = "frc8724";
+const TEAM_KEY = process.env.TEAM_KEY || "frc8724";
 
 app.use(
   cors({ origin: ["http://localhost:4000", "https://mayhemrobotics.org"] })
@@ -21,25 +21,36 @@ app.get("/", (req, res) => {
   res.redirect("https://mayhemrobotics.org");
 });
 
-app.get("/event", async (req, res) => {
+app.get("/currentEvent", async (req, res) => {
+  const now = DateTime.now();
+
   try {
-    const { data: event } = await axios(
-      `https://www.thebluealliance.com/api/v3/event/${EVENT_KEY}`,
+    const { data: events } = await axios(
+      `https://www.thebluealliance.com/api/v3/team/${TEAM_KEY}/events/${now.year}`,
       {
         headers: { "X-TBA-Auth-Key": process.env.TBA_API_KEY! },
       }
     );
 
+    const event = events.find((e: any) => {
+      const start = DateTime.fromSQL(e.start_date);
+      const end = DateTime.fromSQL(e.end_date);
+
+      return now > start && now < end;
+    });
+
+    if (!event) throw null;
+
     res.json(event);
   } catch (e) {
-    res.status(500).json({ ok: false });
+    res.status(404).end();
   }
 });
 
-app.get("/matches", async (req, res) => {
+app.get("/event/:event/matches", async (req, res) => {
   try {
     const { data: matches } = await axios(
-      `https://www.thebluealliance.com/api/v3/team/${TEAM_KEY}/event/${EVENT_KEY}/matches`,
+      `https://www.thebluealliance.com/api/v3/team/${TEAM_KEY}/event/${req.params.event}/matches/simple`,
       {
         headers: { "X-TBA-Auth-Key": process.env.TBA_API_KEY! },
       }
@@ -47,16 +58,16 @@ app.get("/matches", async (req, res) => {
 
     res.json(matches);
   } catch (e) {
-    res.status(500).json({ ok: false });
+    res.status(404).end();
   }
 });
 
-app.get("/rankings", async (req, res) => {
+app.get("/event/:event/rankings", async (req, res) => {
   try {
     const {
       data: { rankings },
     } = await axios(
-      `https://www.thebluealliance.com/api/v3/event/${EVENT_KEY}/rankings`,
+      `https://www.thebluealliance.com/api/v3/event/${req.params.event}/rankings`,
       {
         headers: { "X-TBA-Auth-Key": process.env.TBA_API_KEY! },
       }
@@ -64,7 +75,7 @@ app.get("/rankings", async (req, res) => {
 
     res.json(rankings);
   } catch (e) {
-    res.status(500).json({ ok: false });
+    res.status(404).end();
   }
 });
 
